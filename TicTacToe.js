f9d6061a1494438af4b8bfbb75515131a7858a02
jphang27 <56161279+jphang27@users.noreply.github.com>
2019-10-11 09:01:06 -0500

    added few things in Javascripts
    I added a few things in Javascripts the evening of yestarday trying to figure out what I can do in order to start on my logic for Javascript, it did not go as I planned and I am submitting to GitHub what I have. Also in order to use Netlify, I need to change my files names to the conventional "index.html" as well ass css to "style.css".

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,3 +1,41 @@
-function myFunction(){
-    document.getElementById("demo").innerHTML = "Hello World!";
-}
+// class Board extends Array{
+//     constructor(...args);
+//     super(args);//When you extend a super(args) MUST have
+//     this.player1 = "X";
+//     this.player2 = "O";
+// }
+
+//Trying the easier route attempt first and hardcoding it in.
+const way2Wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], 
+                  [6, 4, 2], [2, 5, 8], [1, 4, 7], [0, 3, 6]];
+
+let sizeOfBoard = 9; //size of board is 3x3
+let score = 0; //keep track of scoring
+let numOfMoves = 0; //check number of moves played
+let turns = 0; //see if it's Player 1 or Player 2 turn.
+
+// setPlayers(playerNum, token){
+//     if(playerNum == 1){
+//         this.player1 = token;
+//     }
+//     else{
+//         this.player2 = token;
+//     }
+// }
+
+/************************************************************************ */
+/***************************TESTING************************************** */
+function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.innerHTML === "X ") {
+      x.innerHTML = "X";
+    } else {
+      x.innerHTML = "O";
+    }
+  }
+/************************************************************************ */
+/************************************************************************ */
+//Used for testing
+// function myFunction(){
+//     document.getElementById("demo").innerHTML = "X";
+// }
